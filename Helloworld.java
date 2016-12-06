d7de84423acf2293bf2553617708094b58b3295a
Mark Goodwin <stocksnut@mac.com>
2016-12-06 13:08:08 -0700

    added new message to the program

--- Helloworld.java
+++ Helloworld.java
@@ -21,6 +21,7 @@ public class Helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+		System.out.println("Here's lookin' at you, kid.")
     }
 
 }
