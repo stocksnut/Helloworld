008b1bd60063107a395a04b304d634abbb666e8f
Mark Goodwin <stocksnut@mac.com>
2016-12-06 13:04:27 -0700

    edited HelloWorld class to be Helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -16,7 +16,7 @@
  *
  ******************************************************************************/
 
-public class HelloWorld {
+public class Helloworld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
