76615d6751fd9e1ff6a39033f644f239725e6324
Mark Goodwin <stocksnut@mac.com>
2017-02-08 08:26:40 -0700

    Added compiled class and fixed semi-colon issue

--- Helloworld.java
+++ Helloworld.java
@@ -22,7 +22,7 @@ public class Helloworld {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 		System.out.println("Here's lookin' at you, kid.");
-		System.out.println("Welcome to New York.")
+		System.out.println("Welcome to New York.");
     }
 
 }
