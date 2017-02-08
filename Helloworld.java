4926882847c9915a061179c7cb756ee8392bd4da
Mark Goodwin <stocksnut@mac.com>
2017-02-08 09:05:48 -0700

    Removed class file and replaced with jar.  Added manifest.txt

--- Helloworld.java
+++ Helloworld.java
@@ -21,7 +21,7 @@ public class Helloworld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
-		System.out.println("Here's lookin' at you, kid.");
+		System.out.println("Here's looking at you, kid.");
 		System.out.println("Welcome to New York.");
     }
 
